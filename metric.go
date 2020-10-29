5c3ac295f1cb147c4af5e7fc6d5f7d59d3b75f76
JBD <jbd@amazon.com>
2020-10-29 00:06:01 -0700

    Changes

--- metric.go
+++ metric.go
@@ -19,36 +19,36 @@ func (m Meter) AsFloat64() Int64Meter {}
 
 type Float64Meter struct{}
 
-func (m Float64Meter) NewCounter(name string, options ...InstrumentOption) (count.Float64Counter, error) {
+func (m Float64Meter) NewCounter(name string, o ...InstrumentOption) (count.Float64Counter, error) {
 }
 
-func (m Float64Meter) NewUpDownCounter(name string, options ...InstrumentOption) (count.Float64UpDownCounter, error) {
+func (m Float64Meter) NewUpDownCounter(name string, o ...InstrumentOption) (count.Float64UpDownCounter, error) {
 }
 
 func (m Float64Meter) NewSumObserver(name string, fn sum.Float64ObserverFunc, options ...InstrumentOption) (sum.Float64SumObserver, error) {
 }
 
-func (m Float64Meter) NewValueRecorder(name string, options ...InstrumentOption) (value.Float64Recorder, error) {
+func (m Float64Meter) NewValueRecorder(name string, o ...InstrumentOption) (value.Float64Recorder, error) {
 }
 
-func (m Float64Meter) NewValueObserver(name string, options ...InstrumentOption) (value.Float64Observer, error) {
+func (m Float64Meter) NewValueObserver(name string, o ...InstrumentOption) (value.Float64Observer, error) {
 }
 
 type Int64Meter struct{}
 
-func (m Int64Meter) NewCounter(name string, options ...InstrumentOption) (count.Int64Counter, error) {
+func (m Int64Meter) NewCounter(name string, o ...InstrumentOption) (count.Int64Counter, error) {
 }
 
-func (m Int64Meter) NewUpDownCounter(name string, options ...InstrumentOption) (count.Int64UpDownCounter, error) {
+func (m Int64Meter) NewUpDownCounter(name string, o ...InstrumentOption) (count.Int64UpDownCounter, error) {
 }
 
-func (m Int64Meter) NewSumObserver(name string, fn sum.Int64ObserverFunc, options ...InstrumentOption) (sum.Int64SumObserver, error) {
+func (m Int64Meter) NewSumObserver(name string, fn sum.Int64ObserverFunc, o ...InstrumentOption) (sum.Int64SumObserver, error) {
 }
 
-func (m Int64Meter) NewValueRecorder(name string, options ...InstrumentOption) (value.Int64Recorder, error) {
+func (m Int64Meter) NewValueRecorder(name string, o ...InstrumentOption) (value.Int64Recorder, error) {
 }
 
-func (m Int64Meter) NewValueObserver(name string, options ...InstrumentOption) (value.Int64Observer, error) {
+func (m Int64Meter) NewValueObserver(name string, o ...InstrumentOption) (value.Int64Observer, error) {
 }
 
 func (m Meter) RecordBatch(ctx context.Context, labels []label.KeyValue, ms ...Measurement) {}
