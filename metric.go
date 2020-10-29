505ed2633d4db8a4720ec08bb773bb02b1ed7c27
JBD <jbd@amazon.com>
2020-10-28 23:26:52 -0700

    Changes

--- metric.go
+++ metric.go
@@ -13,15 +13,37 @@ type Measurement = metric.Measurement
 type Meter struct{}
 
 func (m Meter) NewFloat64Counter(name string, options ...InstrumentOption) (count.Float64Counter, error) {
-	panic("not eyet")
 }
 
 func (m Meter) NewFloat64UpDownCounter(name string, options ...InstrumentOption) (count.Float64UpDownCounter, error) {
-	panic("not eyet")
+}
+
+func (m Meter) NewInt64Counter(name string, options ...InstrumentOption) (count.Int64Counter, error) {
+}
+
+func (m Meter) NewInt64UpDownCounter(name string, options ...InstrumentOption) (count.Int64UpDownCounter, error) {
+}
+
+func (m Meter) NewFloat64SumObserver(name string, fn sum.Float64ObserverFunc, options ...InstrumentOption) (sum.Float64SumObserver, error) {
+}
+
+func (m Meter) NewInt64SumObserver(name string, fn sum.Int64ObserverFunc, options ...InstrumentOption) (sum.Int64SumObserver, error) {
+}
+
+func (m Meter) NewFloat64ValueRecorder(name string, options ...InstrumentOption) (sum.Float64SumObserver, error) {
+}
+
+func (m Meter) NewFloat64ValueObserver(name string, options ...InstrumentOption) (sum.Int64SumObserver, error) {
+}
+
+func (m Meter) NewInt64ValueRecorder(name string, options ...InstrumentOption) (sum.Float64SumObserver, error) {
+}
+
+func (m Meter) NewInt64ValueObserver(name string, options ...InstrumentOption) (sum.Int64SumObserver, error) {
 }
 
 func (m Meter) RecordBatch(ctx context.Context, ls []label.KeyValue, ms ...Measurement) {}
 
-type BatchObserverResult struct{}
+type BatchObserverResult = metric.BatchObserverResult
 
 func (m Meter) NewBatchObserver(fn func(result BatchObserverResult)) {}
--- sum/sum.go
+++ sum/sum.go
@@ -7,3 +7,7 @@ type Float64SumObserver {}
 type Int64UpDownSumObserver{}
 
 type Float64UpDownSumObserver{}
+
+type Float64ObserverFunc func()
+
+type Int64ObserverFunc func()
